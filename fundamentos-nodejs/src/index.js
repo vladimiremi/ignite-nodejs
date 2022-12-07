bc32bacc198cd5df36a6e826c990942e37000cea
vladimiremi <vladimiremi2014@gmail.com>
2022-12-07 14:41:01 -0300

    chore: add nodemon

--- fundamentos-nodejs/src/index.js
+++ fundamentos-nodejs/src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (request, response) => {
-  return response.json({ message: "Hello World" });
+  return response.json({ message: "Hello World Ignite - Fundamentos NodeJs" });
 });
 
 app.listen(3333);
