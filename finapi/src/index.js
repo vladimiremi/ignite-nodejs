dd8ce800ef2487c36104e730a41a83ff126ca656
vladimiremi <vladimiremi2014@gmail.com>
2022-12-07 15:55:26 -0300

    feat: add finapi

---
+++ finapi/src/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const { v4: uuidv4 } = require("uuid");
+
+const app = express();
+
+app.use(express.json());
+
+const customers = [];
+
+app.post("/account", (request, response) => {
+  const { cpf, name, uuid } = request.body;
+
+  const id = uuidv4();
+
+  customers.push({
+    cpf,
+    name,
+    id,
+    statement: [],
+  });
+
+  return response.send();
+});
+
+app.listen(3333);
