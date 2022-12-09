028f378e7514ae544b56bfb55635866072d2f817
vladimiremi <vladimiremi2014@gmail.com>
2022-12-09 07:11:30 -0300

    feat: add validation already exists

--- finapi/src/index.js
+++ finapi/src/index.js
@@ -8,14 +8,20 @@ app.use(express.json());
 const customers = [];
 
 app.post("/account", (request, response) => {
-  const { cpf, name, uuid } = request.body;
+  const { cpf, name } = request.body;
 
-  const id = uuidv4();
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
+
+  if (customerAlreadyExists) {
+    return response.status(400).json({ error: "Customer already Exists!" });
+  }
 
   customers.push({
     cpf,
     name,
-    id,
+    id: uuidv4(),
     statement: [],
   });
 
