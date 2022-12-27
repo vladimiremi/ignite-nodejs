57260b1e8e4ecf864be6f10402b34d0eea334c1e
vladimiremi <vladimiremi2014@gmail.com>
2022-12-27 14:36:59 -0300

    feat: add code debug

--- create-api-nodeJs/src/server.ts
+++ create-api-nodeJs/src/server.ts
@@ -2,4 +2,20 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
+app.post("/courses", (request, response) => {
+  const { name } = request.body;
+  function mimi() {
+    let teste = "1";
+    let boolean = true;
+
+    return false;
+  }
+
+  mimi();
+
+  return response.json({ name });
+});
+
 app.listen(3333, () => console.log("Server is running!🚀🚀"));
