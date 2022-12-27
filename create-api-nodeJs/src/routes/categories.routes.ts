8bf8b151d3b4d2d59bedb127da41bc9ac955f1b2
vladimiremi <vladimiremi2014@gmail.com>
2022-12-27 15:32:15 -0300

    feat: add model category

--- create-api-nodeJs/src/routes/categories.routes.ts
+++ create-api-nodeJs/src/routes/categories.routes.ts
@@ -1,16 +1,18 @@
 import { Router } from "express";
 import { v4 as uuidV4 } from "uuid";
+import { Category } from "../model/Category";
 
 const categoriesRouter = Router();
 
-const categories = [];
+const categories: Category[] = [];
 
 categoriesRouter.post("/", (request, response) => {
   const { name, description } = request.body;
-  const category = {
+  const category: Category = {
     name,
     description,
     id: uuidV4(),
+    created_at: new Date(),
   };
 
   categories.push(category);
