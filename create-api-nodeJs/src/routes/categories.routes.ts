24aed66141cda1c202ea4898f348fad1001bdf6e
vladimiremi <vladimiremi2014@gmail.com>
2022-12-27 15:25:02 -0300

    feat: add uuid and add initial path categories

--- create-api-nodeJs/src/routes/categories.routes.ts
+++ create-api-nodeJs/src/routes/categories.routes.ts
@@ -1,16 +1,20 @@
 import { Router } from "express";
+import { v4 as uuidV4 } from "uuid";
 
 const categoriesRouter = Router();
 
 const categories = [];
 
-categoriesRouter.post("/categories", (request, response) => {
+categoriesRouter.post("/", (request, response) => {
   const { name, description } = request.body;
-  categories.push({
+  const category = {
     name,
     description,
-  });
-  return response.status(200).send();
+    id: uuidV4(),
+  };
+
+  categories.push(category);
+  return response.status(200).json(category);
 });
 
 export { categoriesRouter };
--- create-api-nodeJs/src/server.ts
+++ create-api-nodeJs/src/server.ts
@@ -5,6 +5,6 @@ const app = express();
 
 app.use(express.json());
 
-app.use(categoriesRouter);
+app.use("/categories", categoriesRouter);
 
 app.listen(3333, () => console.log("Server is running!🚀🚀"));
