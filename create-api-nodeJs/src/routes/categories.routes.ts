335ae4c8ac5c0dfc74064df6d017c05d792eb438
vladimiremi <vladimiremi2014@gmail.com>
2022-12-30 07:07:12 -0300

    feat: add Liskov substitution principle

--- create-api-nodeJs/src/routes/categories.routes.ts
+++ create-api-nodeJs/src/routes/categories.routes.ts
@@ -2,11 +2,12 @@ import { Router } from "express";
 
 import { Category } from "../model/Category";
 import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { PostgresCategoriesRepository } from "../repositories/PostgresCategoriesRepository";
 import { CreateCategoryService } from "../services/CreateCategoryService";
 
 const categoriesRouter = Router();
 
-const categoriesRepository = new CategoriesRepository();
+const categoriesRepository = new PostgresCategoriesRepository();
 
 categoriesRouter.post("/", (request, response) => {
   const { name, description } = request.body;
