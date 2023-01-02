a954d8a3bd33f022da653341e0db533b52a77dec
vladimiremi <vladimiremi2014@gmail.com>
2023-01-02 11:04:34 -0300

    feat: create categories Use Case

--- create-api-nodeJs/src/routes/categories.routes.ts
+++ create-api-nodeJs/src/routes/categories.routes.ts
@@ -1,18 +1,15 @@
 import { Router } from "express";
 
 import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepository";
-import { CreateCategoryService } from "../modules/cars/services/CreateCategoryService";
+import { createCategoryController } from "../modules/cars/useCases/createCategory";
+import { CreateCategoryUseCase } from "../modules/cars/useCases/createCategory/CreateCategoryUseCase";
 
 const categoriesRouter = Router();
 
 const categoriesRepository = new CategoriesRepository();
 
 categoriesRouter.post("/", (request, response) => {
-  const { name, description } = request.body;
-  const createCategoryService = new CreateCategoryService(categoriesRepository);
-  const category = createCategoryService.execute({ name, description });
-
-  return response.status(200).json(category);
+  return createCategoryController.handle(request, response);
 });
 
 categoriesRouter.get("/", (request, response) => {
