322b9c0373f715f78bfef520794217cf3515c0d9
vladimiremi <vladimiremi2014@gmail.com>
2023-01-02 14:54:31 -0300

    feat: refactor the categories list

--- create-api-nodeJs/src/routes/categories.routes.ts
+++ create-api-nodeJs/src/routes/categories.routes.ts
@@ -1,21 +1,16 @@
 import { Router } from "express";
 
-import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepository";
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
-import { CreateCategoryUseCase } from "../modules/cars/useCases/createCategory/CreateCategoryUseCase";
+import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 const categoriesRouter = Router();
-
-const categoriesRepository = new CategoriesRepository();
 
 categoriesRouter.post("/", (request, response) => {
   return createCategoryController.handle(request, response);
 });
 
 categoriesRouter.get("/", (request, response) => {
-  const all = categoriesRepository.list();
-
-  return response.json(all);
+  return listCategoriesController.handle(request, response);
 });
 
 export { categoriesRouter };
