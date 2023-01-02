3d35efcc0be94f8c6c19dc913e32eaad05e6bd99
vladimiremi <vladimiremi2014@gmail.com>
2023-01-02 15:11:17 -0300

    feat: use Singleton Pattern

--- create-api-nodeJs/src/modules/cars/repositories/CategoriesRepository.ts
+++ create-api-nodeJs/src/modules/cars/repositories/CategoriesRepository.ts
@@ -7,8 +7,18 @@ import {
 class CategoriesRepository implements ICategoriesRepository {
   private categories: Category[];
 
+  private static INSTANCE: CategoriesRepository;
+
   constructor() {
     this.categories = [];
+  }
+
+  public static getInstance(): CategoriesRepository {
+    if (!CategoriesRepository.INSTANCE) {
+      CategoriesRepository.INSTANCE = new CategoriesRepository();
+    }
+
+    return CategoriesRepository.INSTANCE;
   }
 
   create({ description, name }: ICreateCategoryDTO) {
