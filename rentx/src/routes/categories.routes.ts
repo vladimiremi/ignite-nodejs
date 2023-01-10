122ca9cfeca8c411f025d2a9819872a5dc5f358d
vladimiremi <vladimiremi2014@gmail.com>
2023-01-10 10:43:47 -0300

    refactor: list and import category

--- rentx/src/routes/categories.routes.ts
+++ rentx/src/routes/categories.routes.ts
@@ -1,9 +1,8 @@
 import { Router } from "express";
 import multer from "multer";
 import { CreateCategoryController } from "../modules/cars/useCases/createCategory/CreateCategoryController";
-
-import { importCategoryController } from "../modules/cars/useCases/importCategory";
-import { listCategoriesController } from "../modules/cars/useCases/listCategories";
+import { ImportCategoryController } from "../modules/cars/useCases/importCategory/ImportCategoryController";
+import { ListCategoriesController } from "../modules/cars/useCases/listCategories/ListCategoriesController";
 
 const categoriesRouter = Router();
 
@@ -12,15 +11,17 @@ const upload = multer({
 });
 
 const createCategoryController = new CreateCategoryController();
+const importCategoryController = new ImportCategoryController();
+const listCategoriesController = new ListCategoriesController();
 
 categoriesRouter.post("/", createCategoryController.handle);
 
-categoriesRouter.get("/", (request, response) => {
-  return listCategoriesController.handle(request, response);
-});
+categoriesRouter.get("/", listCategoriesController.handle);
 
-categoriesRouter.post("/import", upload.single("file"), (request, response) => {
-  importCategoryController.execute(request, response);
-});
+categoriesRouter.post(
+  "/import",
+  upload.single("file"),
+  importCategoryController.handle
+);
 
 export { categoriesRouter };
