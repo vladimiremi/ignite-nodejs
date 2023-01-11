565bfc2f657613e61b7db144826e042611c8ec1e
vladimiremi <vladimiremi2014@gmail.com>
2023-01-11 07:09:35 -0300

    feat: add users routes

--- rentx/src/routes/index.ts
+++ rentx/src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { categoriesRouter } from "./categories.routes";
 import { specificationsRouter } from "./specifications.routes";
+import { usersRouters } from "./users.routes";
 
 const router = Router();
 
 router.use("/categories", categoriesRouter);
 router.use("/specifications", specificationsRouter);
+router.use("/users", usersRouters);
 
 export { router };
