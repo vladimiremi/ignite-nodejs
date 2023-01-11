a17a640ddc336956f6cafb7954f0148d1e36526e
vladimiremi <vladimiremi2014@gmail.com>
2023-01-11 08:01:46 -0300

    refactor: remove username

--- rentx/src/modules/accounts/useCases/createUser/CreateUserController.ts
+++ rentx/src/modules/accounts/useCases/createUser/CreateUserController.ts
@@ -4,14 +4,13 @@ import { CreateUserUseCase } from "./CreateUserUseCase";
 
 class CreateUserController {
   async handle(request: Request, response: Response): Promise<Response> {
-    const { name, email, password, username, driver_license } = request.body;
+    const { name, email, password, driver_license } = request.body;
     const createUserUseCase = container.resolve(CreateUserUseCase);
 
     await createUserUseCase.execute({
       name,
       email,
       password,
-      username,
       driver_license,
     });
 
--- rentx/src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ rentx/src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -12,14 +12,12 @@ class CreateUserUseCase {
     name,
     email,
     password,
-    username,
     driver_license,
   }: ICreateUserDTO): Promise<void> {
     await this.usersRepository.create({
       name,
       email,
       password,
-      username,
       driver_license,
     });
   }
