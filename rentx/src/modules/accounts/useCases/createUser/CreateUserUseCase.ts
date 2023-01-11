ab61043f1c73d21f766ddd54f53fa95b54adc647
vladimiremi <vladimiremi2014@gmail.com>
2023-01-11 07:08:44 -0300

    feat: add create user use case

---
+++ rentx/src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,28 @@
+import { inject, injectable } from "tsyringe";
+import { ICreateUserDTO } from "../../dto/ICreateUserDTO";
+import { IUsersRepository } from "../../repositories/IUserRepository";
+
+@injectable()
+class CreateUserUseCase {
+  constructor(
+    @inject("UserRepository") private usersRepository: IUsersRepository
+  ) {}
+
+  async execute({
+    name,
+    email,
+    password,
+    username,
+    driver_license,
+  }: ICreateUserDTO): Promise<void> {
+    await this.usersRepository.create({
+      name,
+      email,
+      password,
+      username,
+      driver_license,
+    });
+  }
+}
+
+export { CreateUserUseCase };
