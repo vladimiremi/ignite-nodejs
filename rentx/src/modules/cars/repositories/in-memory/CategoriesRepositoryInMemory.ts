fa71952ae1cdb81a111869763aa3626c24cef81a
vladimiremi <vladimiremi2014@gmail.com>
2023-01-14 10:26:59 -0300

    feat: add category repository in-memory

---
+++ rentx/src/modules/cars/repositories/in-memory/CategoriesRepositoryInMemory.ts
@@ -0,0 +1,32 @@
+import { Category } from "../../entities/Category";
+import {
+  ICategoriesRepository,
+  ICreateCategoryDTO,
+} from "../ICategoriesRepository";
+
+class CategoriesRepositoryInMemory implements ICategoriesRepository {
+  categories: Category[] = [];
+
+  async findByName(name: string): Promise<Category> {
+    const category = this.categories.find((category) => category.name === name);
+    return category;
+  }
+  async list(): Promise<Category[]> {
+    const all = this.categories;
+    return all;
+  }
+  async create({ description, name }: ICreateCategoryDTO): Promise<Category> {
+    const category = new Category();
+
+    Object.assign(category, {
+      description,
+      name,
+    });
+
+    this.categories.push(category);
+
+    return category;
+  }
+}
+
+export { CategoriesRepositoryInMemory };
