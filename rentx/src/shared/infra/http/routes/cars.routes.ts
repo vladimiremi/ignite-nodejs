af65cb82cf8f7c6637c5209ccd9d9e17586e0e5c
vladimiremi <vladimiremi2014@gmail.com>
2023-01-19 08:42:33 -0300

    feat: add list available cars

--- rentx/src/shared/infra/http/routes/cars.routes.ts
+++ rentx/src/shared/infra/http/routes/cars.routes.ts
@@ -1,4 +1,5 @@
 import { CreateCarController } from "@modules/cars/useCases/createCar/CreateCarController";
+import { ListAvailableController } from "@modules/cars/useCases/listAvailableCars/listAvailableCarsController";
 import { Router } from "express";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 import ensuredAdmin from "../middlewares/ensuredAdmin";
@@ -6,6 +7,7 @@ import ensuredAdmin from "../middlewares/ensuredAdmin";
 const carsRouters = Router();
 
 const createCarController = new CreateCarController();
+const listAvailableController = new ListAvailableController();
 
 carsRouters.post(
   "/",
@@ -13,5 +15,7 @@ carsRouters.post(
   ensuredAdmin,
   createCarController.handle
 );
+
+carsRouters.get("/available", listAvailableController.handle);
 
 export { carsRouters };
