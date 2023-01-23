a41b129c9d7a13cdd8559cd3cf80c9cd216d09f2
vladimiremi <vladimiremi2014@gmail.com>
2023-01-23 11:47:02 -0300

    feat: add migration rentals

--- rentx/src/shared/infra/typeorm/migrations/1673376244799-CreateUsers.ts
+++ rentx/src/shared/infra/typeorm/migrations/1673376244799-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1673376244799 implements MigrationInterface {
           {
             name: "id",
             type: "uuid",
+            isPrimary: true,
           },
           {
             name: "name",
