47cc2af782ddf8f7e59f803fbc2e2dc82f9199d6
vladimiremi <vladimiremi2014@gmail.com>
2023-01-24 13:46:23 -0300

    feat: add rentals dependency injection

--- rentx/src/shared/infra/http/routes/rental.routes.ts
+++ rentx/src/shared/infra/http/routes/rental.routes.ts
@@ -1,10 +1,10 @@
-import { CreateCarController } from "@modules/cars/useCases/createCar/CreateCarController";
+import { CreateRentalController } from "@modules/rentals/useCases/createRental/CreateRentalController";
 import { Router } from "express";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const rentalRoutes = Router();
 
-const createRentalController = new CreateCarController();
+const createRentalController = new CreateRentalController();
 
 rentalRoutes.post("/", ensureAuthenticated, createRentalController.handle);
 
