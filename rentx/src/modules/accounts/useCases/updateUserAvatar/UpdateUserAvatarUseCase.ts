69d84db2afe5ae2d1bbc5e996d898aeeec80703d
vladimiremi <vladimiremi2014@gmail.com>
2023-02-10 08:46:06 -0300

    refactor: change structure for accepting send file to s3, too

--- rentx/src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ rentx/src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -1,7 +1,7 @@
 import { inject, injectable } from "tsyringe";
 
-import { deleteFile } from "@utils/file";
 import { UserRepository } from "@modules/accounts/infra/typeorm/repositories/UserRepository";
+import { IStorageProvider } from "@shared/providers/StorageProvider/IStorageProvider";
 
 interface IRequest {
   user_id: string;
@@ -11,14 +11,17 @@ interface IRequest {
 @injectable()
 class UpdateUserAvatarUseCase {
   constructor(
-    @inject("UserRepository") private userRepository: UserRepository
+    @inject("UserRepository") private userRepository: UserRepository,
+    @inject("StorageProvider")
+    private storageProvider: IStorageProvider
   ) {}
   async execute({ avatar_file, user_id }: IRequest) {
     const user = await this.userRepository.findById(user_id);
 
     if (user.avatar) {
-      await deleteFile(`./tmp/avatar/${user.avatar}`);
+      await this.storageProvider.delete(user.avatar, "avatar");
     }
+    await this.storageProvider.save(avatar_file, "avatar");
 
     user.avatar = avatar_file;
 
