e29ef63cbe3606412473073080eff1f65516268b
vladimiremi <vladimiremi2014@gmail.com>
2023-02-13 08:06:07 -0300

    feat: add bucket s3

--- rentx/src/shared/providers/index.ts
+++ rentx/src/shared/providers/index.ts
@@ -1,8 +1,9 @@
 import { container } from "tsyringe";
 import { LocalStorageProvider } from "./StorageProvider/implementations/LocalStorageProvider";
+import { S3StorageProvider } from "./StorageProvider/implementations/S3StorageProvider";
 import { IStorageProvider } from "./StorageProvider/IStorageProvider";
 
 container.registerSingleton<IStorageProvider>(
   "StorageProvider",
-  LocalStorageProvider
+  S3StorageProvider
 );
